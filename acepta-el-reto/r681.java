e31e70d283b2347ec855244f6483fdf1a2b768f2
VictorMerino2002 <merino.rodriguez.victor@gmail.com>
2024-02-15 20:52:09 +0100

    La escalera de Alba

---
+++ acepta-el-reto/r681.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class r681 {
+
+    public static Scanner input  = new Scanner(System.in);
+
+    public static int[] getPieces(int numOfPieces) {
+        int[] pieces = new int[numOfPieces];
+
+        for (int i=0;i<numOfPieces;i++) pieces[i] = input.nextInt();
+        return pieces;
+    }
+
+    public static void displayMaxNumOfPieces(int[] pieces) {
+        Arrays.sort(pieces);
+        long sum = pieces[pieces.length - 1];
+
+        for (int i = pieces.length - 2; i >= 0; i--) {
+            if (pieces[i + 1] <= 1) break;
+            int parts = pieces[i];
+            if (pieces[i] >= pieces[i + 1]) parts = pieces[i + 1] - 1;
+            pieces[i] = parts;
+            sum += parts;
+        }
+
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        while (input.hasNext()) {
+            int numOfPieces = input.nextInt();
+
+            int[] pices = getPieces(numOfPieces);
+            displayMaxNumOfPieces(pices);
+        }
+    }
+}
